d97b6dbaf98b4898c3994ec2b28a4edd38e8f791
zhangyifeng <291323003@qq.com>
2019-08-08 01:32:04 +0800

    up

---
+++ src/Queue.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace easySwoole\Queue;
+
+
+class Queue
+{
+    public function push()
+    {
+
+    }
+
+    public function pop()
+    {
+
+    }
+
+    public function later()
+    {
+
+    }
+
+    public function size()
+    {
+
+    }
+}
