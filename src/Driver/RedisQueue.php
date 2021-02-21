a96bafaf176588148914fa99208da1d4608a2d8b
如果的如果 <admin@fosuss.com>
2021-02-21 17:04:33 +0800

    rebuild

--- src/Driver/RedisQueue.php
+++ src/Driver/RedisQueue.php
@@ -6,9 +6,20 @@ namespace EasySwoole\Queue\Driver;
 
 use EasySwoole\Queue\Job;
 use EasySwoole\Queue\QueueDriverInterface;
+use EasySwoole\Redis\Config\RedisConfig;
+use EasySwoole\RedisPool\Pool;
 
 class RedisQueue implements QueueDriverInterface
 {
+    protected $pool;
+    protected $queueName;
+
+    public function __construct(RedisConfig $config,string $queueName = 'easy_queue')
+    {
+        $this->pool = new Pool($config);
+        $this->queueName = $queueName;
+    }
+
     public function push(Job $job): bool
     {
         // TODO: Implement push() method.
